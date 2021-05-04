7b07577067dd4a8275c604d8581a39044dd7dd0e
B-10515025 <ddt22771508@gmail.com>
2021-05-04 20:31:11 +0800

    Router header and implement

---
+++ src/Components/Router.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "Component.h"
+#include <vector>
+#include <Winsock2.h>
+
+class Controller;
+class Decoder;
+
+class Router: public Component<H264> {
+public:
+    Router(Decoder*, Controller*);
+    bool setPort(int);
+    bool link(string, int, int);
+protected:
+    void start();
+    bool wait();
+    void process();
+    void end();
+private:
+    Decoder* decoder;
+    Controller* controller;
+    atomic_bool living;
+    SOCKET upStream, entry;
+    vector<SOCKET> downStream;
+    mutex vecMtx;
+    void server();
+};
